14cbe224852320cb315ed38ca94878deb8e28689
Stefan <stefan.r95@hotmail.com>
2021-03-14 18:50:49 +0100

    AAAAA

--- src/main/java/com/stefan/programacionestructurada/sentenciasCondicionales.java
+++ src/main/java/com/stefan/programacionestructurada/sentenciasCondicionales.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templatesaaaa
- * and open the template in the editor.
+ * and open the template in the editor.aaaa
  */
 package com.stefan.programacionestructurada;
 
